8d2f46b1365cc25ba7dcb479518172cbb2a35da4
aborn <loveaborn@foxmail.com>
2024-10-08 20:05:40 +0800

    testbutton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <DemoButton size="large" type="primary">我是测试组件</DemoButton>
+  <TestButton size="middle" type="primary">我是测试组件2</TestButton>
 </template>
 
 <script lang="ts" setup>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import DemoUI from "./components/demo-ui"; //导入
+import FormGenerator from "./components/form-generator"
 
 const app = createApp(App);
 app.use(DemoUI)
+app.use(FormGenerator)
 app.mount('#app')
