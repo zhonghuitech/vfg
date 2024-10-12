84218748caf07e06a0d1b2a79976b05864f3c456
aborn <loveaborn@foxmail.com>
2024-10-12 20:11:51 +0800

    使用VFG简称

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'; // 三方依赖
 import 'element-plus/theme-chalk/index.css';
 import SvgIcon from '@/components/SvgIcon/index.vue'
-import elementIcons from '@/components/SvgIcon/svgicon'
+import * as ElIcon from '@element-plus/icons-vue'
 import { VueDraggableNext } from "vue-draggable-next";
-import DraggableWarp from "@/components/form-generator/form/DraggableWarp.vue";
-import OptionInput from "@/components/form-generator/form/OptionInput.vue";
-import UploadWarp from "@/components/form-generator/form/elementWarp/UploadWarp.vue";
+import DraggableWarp from "@/components/VFG/form/DraggableWarp.vue";
+import OptionInput from "@/components/VFG/form/OptionInput.vue";
+import UploadWarp from "@/components/VFG/form/elementWarp/UploadWarp.vue";
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/googlecode.css' //样式文件
 
 import DemoUI from "./components/demo-ui"; //导入
-import FormBuilder from "./components/form-generator"
+import { VFG } from "./components/VFG"
 
 const app = createApp(App);
 app.use(ElementPlus, {
@@ -22,13 +22,15 @@ app.use(ElementPlus, {
 });
 
 app.use(DemoUI)
-app.use(FormBuilder)
+app.use(VFG)
 
 app.component("draggable", VueDraggableNext as any);
 app.component("draggable-warp", DraggableWarp as any);
 app.component("option-input", OptionInput as any);
 import 'virtual:svg-icons-register'
-app.use(elementIcons)
+Object.keys(ElIcon).forEach((key) => {
+    app.component(key, ElIcon[key as keyof typeof ElIcon])
+})
 app.component("svg-icon", SvgIcon as any);
 app.component("upload-warp", UploadWarp as any);
 
