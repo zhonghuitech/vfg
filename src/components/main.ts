06e87a1a334a4bfb2e554462b61be5ea03393d46
aborn <loveaborn@foxmail.com>
2024-10-12 21:55:46 +0800

    version 0.0.4

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,4 +1,7 @@
 import { VFG } from "./VFG/index"
-import SvgIcon from './SvgIcon/index.vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+import DraggableWarp from "@/components/VFG/form/DraggableWarp.vue";
+import OptionInput from "@/components/VFG/form/OptionInput.vue";
+import UploadWarp from "@/components/VFG/form/elementWarp/UploadWarp.vue";
 
-export { VFG, SvgIcon }
+export { VFG, SvgIcon, DraggableWarp, OptionInput, UploadWarp }
