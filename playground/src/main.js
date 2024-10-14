59af3102dfd0a9d3dec2f456fdbfcab738541683
aborn <loveaborn@foxmail.com>
2024-10-14 22:30:43 +0800

    update deps.

--- playground/src/main.js
+++ playground/src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'; // 三方依赖
 import 'element-plus/theme-chalk/index.css';
 import * as ElIcon from '@element-plus/icons-vue'
-import { VueDraggableNext } from "vue-draggable-next";
+import { VueDraggable } from 'vue-draggable-plus'
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/googlecode.css' //样式文件
 
-import { VFG, SvgIcon, DraggableWarp, OptionInput, UploadWrap } from "vfg"
+import { VFG, SvgIcon, OptionInput, UploadWrap } from "vfg"
 import 'vfg/dist/style.css';
 
 const app = createApp(App);
@@ -19,8 +19,7 @@ app.use(ElementPlus, {
 
 app.use(VFG)
 app.component("svg-icon", SvgIcon);
-app.component("draggable", VueDraggableNext);
-app.component("draggable-warp", DraggableWarp);
+app.component("draggable", VueDraggable);
 app.component("option-input", OptionInput);
 import 'virtual:svg-icons-register'
 Object.keys(ElIcon).forEach((key) => {
