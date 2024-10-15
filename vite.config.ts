a0b2b5dc2ae2e00efd224e60f601f79478ab87e4
aborn <loveaborn@foxmail.com>
2024-10-15 20:32:12 +0800

    delete useless

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,8 @@ export default defineConfig(({ mode, command }) => {
     },
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        "/@": fileURLToPath(new URL('./src', import.meta.url)),
       }
     }
   }
