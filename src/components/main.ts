530d4d2b58eb6211f3712d1fb21c7bad21ff2696
aborn <loveaborn@foxmail.com>
2024-10-15 11:43:32 +0800

    0.0.6

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,11 +1,10 @@
 import type { App } from 'vue';
 import { VFG, OptionInput, UploadWrap } from "./VFG/index"
 import SvgIcon from '@/components/SvgIcon/index.vue'
-import 'vfg/dist/style.css';
 import 'virtual:svg-icons-register'
 
 export { VFG, SvgIcon, OptionInput, UploadWrap }
 export function setupVFG(app: App<Element>) {
   app.use(VFG)
-  app.component("svg-icon", SvgIcon)
+  app.component("svg-icon", SvgIcon as any)
 }
--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,7 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx' // 添加这一句
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 import createSvgIcon from './svg-icon'
 
 export default function createVitePlugins(viteEnv: any, mode: any, isBuild: boolean = false) {
