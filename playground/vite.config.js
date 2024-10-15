053b83104cb51b210f071c1418e083cadf92fed3
aborn <loveaborn@foxmail.com>
2024-10-15 19:55:59 +0800

    playground config base

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -4,15 +4,18 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/vfg/",
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [
-        path.resolve(process.cwd(), 'node_modules/vfg/dist/assets/icons'),
-        path.resolve(process.cwd(), 'src/assets/icons')
-      ],
-      symbolId: 'icon-[dir]-[name]',
-    })]
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: command === 'build' ? '/vfg/' : '',
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [
+          path.resolve(process.cwd(), 'node_modules/vfg/dist/assets/icons'),
+          path.resolve(process.cwd(), 'src/assets/icons')
+        ],
+        symbolId: 'icon-[dir]-[name]',
+      })
+    ]
+  }
 })
