474aca5c5bfd6de84b3003bf0a2aba5dc4483581
aborn <loveaborn@foxmail.com>
2024-10-28 11:09:05 +0800

    vite path move to build.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import path from "path";
 
 import { defineConfig, loadEnv } from 'vite'
-import createVitePlugins from "./vite/plugins/index";
+import createVitePlugins from "./build/plugins/index";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
