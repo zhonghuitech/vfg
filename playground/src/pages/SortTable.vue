ce7f62837f59aee865a59903f6ca45c957a2e6bf
aborn <loveaborn@foxmail.com>
2024-11-01 15:25:38 +0800

    compare.

--- playground/src/pages/SortTable.vue
+++ playground/src/pages/SortTable.vue
@@ -1,25 +1,52 @@
 <template>
     <div>
-        <el-table class="elTable" :data="dList">
-            <el-table-column type="selection" width="55" align="center" />
-            <el-table-column label="排序号" width="150px" align="center" fixed prop="num" />
-            <el-table-column label="姓名" fixed align="center" prop="name" />
-            <el-table-column label="操作" align="center" fixed="right" width="150px"
-                class-name="small-padding fixed-width">
-                <template #default="scope">
-                    <el-button type="primary" link icon="Edit" @click="handleUpdate(scope.row)">修改</el-button>
-                    <el-button type="danger" link icon="Delete" @click="handleDelete(scope.row)">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+        <el-row style="margin-top:50px" align="middle" group="componentsGroup" animation="340" :gutter="24">
+            <el-col :span="12">
+                <div style="display: flex;align-items: center;justify-content: center;margin-bottom: 10px;">
+                    <span style="color: orange;">实际效果</span>
+                </div>
 
-        <el-button style="margin-left: 50px;margin-top: 20px;" type="primary" @click="savAction">Save</el-button>
+                <el-table ref="tabA" class="elTable" :data="dList">
+                    <el-table-column label="排序号" width="150px" align="center" fixed prop="num" />
+                    <el-table-column label="姓名" fixed align="center" prop="name" />
+                    <el-table-column label="操作" align="center" fixed="right" width="150px"
+                        class-name="small-padding fixed-width">
+                        <template #default="scope">
+                            <el-button type="primary" link icon="Edit" @click="handleUpdate(scope.row)">修改</el-button>
+                            <el-button type="danger" link icon="Delete" @click="handleDelete(scope.row)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+            <el-col :span="12" style="border-left: 1px solid #409eff;">
+                <div style="display: flex;align-items: center;justify-content: center;margin-bottom: 10px;">
+                    <span style="color: green;">期望效果</span>
+                </div>
+
+                <el-table ref="tabB" :data="newList">
+                    <el-table-column label="排序号" width="150px" align="center" fixed prop="num" />
+                    <el-table-column label="姓名" fixed align="center" prop="name" />
+                    <el-table-column label="操作" align="center" fixed="right" width="150px"
+                        class-name="small-padding fixed-width">
+                        <template #default="scope">
+                            <el-button type="primary" link icon="Edit" @click="handleUpdate(scope.row)">修改</el-button>
+                            <el-button type="danger" link icon="Delete" @click="handleDelete(scope.row)">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+        </el-row>
+
+        <div style="display: flex;align-items: center;justify-content: center;margin-top: 10px;">
+            <el-button style="margin-left: 50px;margin-top: 20px;" type="primary" @click="savAction">应用排序</el-button>
+        </div>
     </div>
+
 </template>
 
 <script setup>
 import Sortable from 'sortablejs';
-import { onMounted, ref, toRaw } from "vue"
+import { onMounted, ref, toRaw, watch } from "vue"
 
 const initArr = [
     { num: 1, name: "N1" },
@@ -30,7 +57,10 @@ const initArr = [
 ]
 const newList = ref(initArr)
 const dList = ref(initArr.map(i => i))
+
 const sortInstance = ref(null)
+const tabA = ref(null)
+const tabB = ref(null)
 
 const initSort = () => {
     console.log('init sort.........')
@@ -43,10 +73,12 @@ const initSort = () => {
         easing: 'cubic-bezier(1, 0, 0, 1)',
         store: null,
         onStart: (item) => {
-            console.log(item);
+            console.log('onstart...');
+            console.log(sortInstance.value.toArray())
         },
         onSort: function (/**Event*/evt) {
             console.log('onsort...')
+            console.log(sortInstance.value.toArray())
         },
         // 结束拖动事件
         onEnd: (item) => {
@@ -56,6 +88,10 @@ const initSort = () => {
         },
     })
 }
+
+watch(dList, (old, newv) => {
+    console.log('dList changed...')
+})
 
 const handleUpdate = (row) => {
 }
@@ -75,9 +111,17 @@ const savAction = () => {
             name: item.name
         }
     })
+    newList.value = reOrList
+
+    console.log('sortable 对象。。。（原生）')
+    console.log(sortInstance.value)
+
     console.log('最新值')
     console.log(reOrList)
     dList.value = reOrList
+
+    console.log('table 对象...(vue)')
+    console.log(tabA.value)
 }
 
 const onEnd = (item) => {
@@ -94,34 +138,10 @@ const onEnd = (item) => {
     console.log('原始 orderlist')
     console.log(initArr)
     newList.value = newOrderList
+    // dList.value = newOrderList
+    console.log(tabA.value)
 }
 
 </script>
 
-<style>
-.el-table__row {
-    cursor: move;
-}
-
-.infinite-list {
-    height: 300px;
-    padding: 0;
-    margin: 0;
-    list-style: none;
-}
-
-.infinite-list .infinite-list-item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 50px;
-    cursor: move;
-    background: var(--el-color-primary-light-9);
-    margin: 10px;
-    color: var(--el-color-primary);
-}
-
-.infinite-list .infinite-list-item+.list-item {
-    margin-top: 10px;
-}
-</style>
+<style></style>
