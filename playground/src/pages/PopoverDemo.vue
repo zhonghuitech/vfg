71f108d591f1fa38b959beb5f22774f6bbc0bfb4
aborn <loveaborn@foxmail.com>
2025-01-21 22:10:01 +0800

    fix

--- playground/src/pages/PopoverDemo.vue
+++ playground/src/pages/PopoverDemo.vue
@@ -1,7 +1,8 @@
 <template>
     <div style="margin: 20px;display:flex;flex-direction:column">
         <div>
-            <span style="font-size: 30px;">el-popover内有select时在选择后会自动关闭示例</span>
+            <span style="font-size: 30px;">el-popover内有select时在选择后会自动关闭示例, <el-button link type="danger"
+                    style="font-size: 30px;" @click="showSourceCode">查看源码</el-button></span>
         </div>
         <div style="margin-top: 20px;display:flex;">
             <el-popover placement="bottom" :width="500" trigger="click">
@@ -122,6 +123,106 @@
                     <el-button type="primary" @click="proBtnClick">优雅解决</el-button>
                 </template>
             </el-popover>
+
+            <el-popover placement="bottom" :width="500" trigger="click" :visible="popoverVisibleNew">
+                <template #default>
+                    <div ref="elPopoverFilterCardWrapDivNewRef">
+                        <el-card style="max-width: 500px;border:0px solid red;--el-card-border-color:#ffffff"
+                            shadow="never">
+                            <template #header>
+                                <div class="card-header">
+                                    <span style="font-weight: bold;font-size: large;">筛选</span>
+                                </div>
+                            </template>
+
+                            <el-form ref="taskRef">
+                                <el-row>
+                                    <el-col :span="12">
+                                        <el-form-item prop="itemCode" style="margin-left: 10px;">
+                                            <el-select v-model="form.type" placeholder="" :teleported="true"
+                                                placement="bottom">
+                                                <el-option v-for="dict in type_dic" :key="dict.value"
+                                                    :label="dict.label" :value="dict.value"></el-option>
+                                            </el-select>
+                                        </el-form-item>
+                                    </el-col>
+
+                                    <el-col :span="12">
+                                        <el-form-item>
+                                            <el-date-picker id="itemValueDataPickerId" v-model="form.itemValue"
+                                                type="datetimerange" range-separator="到" start-placeholder="开始时间"
+                                                end-placeholder="结束时间" :shortcuts="shortcuts" :teleported="true" />
+                                        </el-form-item>
+                                    </el-col>
+                                </el-row>
+                            </el-form>
+
+                            <template #footer>
+                                <div style="display: flex;justify-content:space-between">
+                                    <span>
+                                        <el-button @click="cancel" link>取 消</el-button>
+                                        <el-button type="primary" @click="submitFilterForm">确 定</el-button>
+                                    </span>
+                                </div>
+                            </template>
+                        </el-card>
+                    </div>
+                </template>
+                <template #reference>
+                    <el-button type="primary" @click="proNewBtnClick">新的问题：el-date-picker</el-button>
+                </template>
+            </el-popover>
+
+            <el-popover placement="bottom" :width="500" trigger="click" :visible="popoverVisibleFinal">
+                <template #default>
+                    <div ref="elPopoverFilterCardWrapDivFinalRef">
+                        <el-card style="max-width: 500px;border:0px solid red;--el-card-border-color:#ffffff"
+                            shadow="never">
+                            <template #header>
+                                <div class="card-header">
+                                    <span style="font-weight: bold;font-size: large;">筛选</span>
+                                </div>
+                            </template>
+
+                            <el-form ref="taskRef">
+                                <el-row>
+                                    <el-col :span="12">
+                                        <el-form-item prop="itemCode" style="margin-left: 10px;">
+                                            <el-select v-model="form.type" placeholder="" :teleported="true"
+                                                placement="bottom">
+                                                <el-option v-for="dict in type_dic" :key="dict.value"
+                                                    :label="dict.label" :value="dict.value"></el-option>
+                                            </el-select>
+                                        </el-form-item>
+                                    </el-col>
+
+                                    <el-col :span="12">
+                                        <el-form-item>
+                                            <el-date-picker id="itemValueDataPickerId"
+                                                popper-class="ItemValueDataPickerPopoverVitualClass"
+                                                v-model="form.itemValue" type="datetimerange" range-separator="到"
+                                                start-placeholder="开始时间" end-placeholder="结束时间" :shortcuts="shortcuts"
+                                                :teleported="true" />
+                                        </el-form-item>
+                                    </el-col>
+                                </el-row>
+                            </el-form>
+
+                            <template #footer>
+                                <div style="display: flex;justify-content:space-between">
+                                    <span>
+                                        <el-button @click="cancelFinal" link>取 消</el-button>
+                                        <el-button type="primary" @click="submitFilterFormFinal">确 定</el-button>
+                                    </span>
+                                </div>
+                            </template>
+                        </el-card>
+                    </div>
+                </template>
+                <template #reference>
+                    <el-button type="success" @click="proFinalBtnClick">最终优雅解决</el-button>
+                </template>
+            </el-popover>
         </div>
     </div>
 </template>
@@ -130,6 +231,9 @@
 import { onMounted, onUnmounted, reactive, ref, toRefs, nextTick } from 'vue';
 
 const popoverVisible = ref(false)
+const popoverVisibleNew = ref(false)
+const popoverVisibleFinal = ref(false)
+
 const data = reactive({
     form: { type: '' }
 })
@@ -137,10 +241,28 @@ const { form } = toRefs(data);
 
 function proBtnClick(e) {
     popoverVisible.value = true
-    e.stopPropagation();    
+    e.stopPropagation();
+}
+
+function proNewBtnClick(e) {
+    popoverVisibleNew.value = true
+    e.stopPropagation();
+}
+
+function proFinalBtnClick(e) {
+    popoverVisibleFinal.value = true
+    e.stopPropagation();
+}
+
+function showSourceCode(e) {
+    window.open('https://github.com/zhonghuitech/vfg/blob/main/playground/src/pages/PopoverDemo.vue', '_blank');
+    e.stopPropagation();
 }
 
 const elPopoverFilterCardWrapDivRef = ref(null)
+const elPopoverFilterCardWrapDivNewRef = ref(null)
+const elPopoverFilterCardWrapDivFinalRef = ref(null)
+
 function submitFilterForm() {
     console.log('submit')
     popoverVisible.value = false
@@ -148,6 +270,14 @@ function submitFilterForm() {
 
 function cancel() {
     popoverVisible.value = false
+}
+
+function cancelFinal() {
+    popoverVisibleFinal.value = false
+}
+
+function submitFilterFormFinal() {
+    popoverVisibleFinal.value = false
 }
 
 const type_dic = [{
@@ -203,7 +333,53 @@ const type_dic = [{
     ismultiple: true
 }]
 
+const shortcuts = [
+    {
+        text: '前一天',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setDate(start.getDate() - 1)
+            return [start, end]
+        },
+    },
+    {
+        text: '前一周',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setDate(start.getDate() - 7)
+            return [start, end]
+        },
+    },
+    {
+        text: '前一月',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setMonth(start.getMonth() - 1)
+            return [start, end]
+        },
+    },
+    {
+        text: '前三月',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setMonth(start.getMonth() - 3)
+            return [start, end]
+        },
+    },
+]
+
 const isPopoverChildComponent = (element, parent) => {
+    if (element && element.classList) {
+        if (element.classList.contains('ItemValueDataPickerPopoverVitualClass')) {
+            console.log('ItemValueDataPickerPopoverVitualClass')
+            return true;
+        }
+    }
+
     if (element === parent) {
         // ref 只能作用于原生 html 组件上，不能在 el-card 之上，所以只能在 el-card 上包一层。
         console.log(element)
@@ -223,6 +399,20 @@ const handleGlobalClick = (event) => {
         } else {
             console.log('点击发生在 Popover 之外');
             popoverVisible.value = false
+        }
+
+        if (isPopoverChildComponent(event.target, elPopoverFilterCardWrapDivNewRef.value)) {
+            console.log('点击发生在 Popover2 或其子组件上');
+        } else {
+            console.log('点击发生在 Popover2 之外');
+            popoverVisibleNew.value = false
+        }
+
+        if (isPopoverChildComponent(event.target, elPopoverFilterCardWrapDivFinalRef.value)) {
+            console.log('点击发生在 Popover3 或其子组件上');
+        } else {
+            console.log('点击发生在 Popover2 之外');
+            popoverVisibleFinal.value = false
         }
     });
 };
